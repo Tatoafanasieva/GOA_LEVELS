cbb007d614282c70aaaaac197e06e43bdeb1e237
Tatoafanasieva <Tamariafanasieva10@gmail.com>
2025-02-24 13:08:26 +0400

    GOA

---
+++ level19/homework/hw2.py
@@ -0,0 +1,10 @@
+# 2️⃣ დაბეჭდეთ "GOA BEST!!!" 100-ჯერ, ორივე ციკლით შეასრულეთ ეს დავალება
+
+for i in range(100):
+    print("GOA BEST!!!")
+    
+x = 0
+
+while x < 100:
+    print ("GOA BEST!!!")
+    x += 1
